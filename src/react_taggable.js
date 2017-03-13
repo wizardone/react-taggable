39edb98be137fe5f56b9c0743af9f25f4ad57677
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-03-13 07:53:30 +0200

    add state to component

--- src/react_taggable.js
+++ src/react_taggable.js
@@ -3,6 +3,11 @@ import ReactDom from 'react-dom';
 
 export default class ReactTaggable extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {tags: []}
+  }
+
   render(){
     return (
       <div id="taggable-div">Tags should be here</div>
